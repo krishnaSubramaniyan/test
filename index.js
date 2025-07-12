fb7cd0c6c090a02bac77f15a7059b50c8c116db3
krishna subramaniyan <krishnasubramaniyan2001@gmail.com>
2025-07-13 00:35:26 +0530

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get('/',(req,res) => res.status(200));
+app.get('/',(req,res) => res.status(200).send("hello world"));
 
 app.get('/api/current-year',(req,res)=>{
     res.send(new Date().getFullYear());
